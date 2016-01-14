c00df990b4cb4a09c00662ae30ca57668a01d3a1
Waiting7777 <waiting0819@msn.com>
2016-01-14 15:40:16 +0800

    tako module done

---
+++ js/vote-tako.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+
+	$('#btn-dist').attr('disabled', true);
+
+	d3.csv('data/dist_list.txt', function(data_dist){
+		
+		mark = '';
+		$('#list-town').empty();
+
+		for(i in data_dist){
+			
+			if(mark != data_dist[i]['town']){
+
+				temp = '<li><a href="#">';
+				temp += data_dist[i]['town'];
+				temp += '</a></li>';
+				mark = data_dist[i]['town'];
+				$('#list-town').append(temp);
+
+			}
+		}
+
+
+		$('#list-town li').click(function(){
+
+			$('#btn-town').text($(this).text());
+			$('#btn-dist').attr('disabled', false);
+
+			dist = $(this).text();
+
+			count = 0;
+
+			d3.csv('data/dist_list.txt', function(data_dist){
+				$('#list-dist').empty();
+				for(i in data_dist){
+
+					if(dist == data_dist[i]['town']){
+						
+						if(count == 0){
+							$('#btn-dist').text(data_dist[i]['dist']);
+							count += 1;
+						}
+
+						temp = '<li><a href="#">';
+						temp += data_dist[i]['dist'];
+						temp += '</a></li>';
+						$('#list-dist').append(temp);
+
+					}
+
+				}
+
+			});
+
+		});
+	});
+
+	$('#list-dist').delegate('li', 'click', function(){
+		$('#btn-dist').text($(this).text());
+	});
+
+	$('#btn-result').click(function(){
+
+		console.log($('#btn-town').text());
+		console.log($('#btn-dist').text());
+
+		temp = $('#btn-town').text() + $('#btn-dist').text();
+
+		d3.csv('data/dist_rank_2012.txt', function(rank_list){
+
+			for(i in rank_list){
+				if(rank_list[i]['town'] == temp){
+					$('#result-2012 .result-rank').text(rank_list[i]['rank']);
+				}
+			}
+
+		});
+
+		d3.csv('data/dist_rank_2008.txt', function(rank_list){
+
+			for(i in rank_list){
+				if(rank_list[i]['town'] == temp){
+					$('#result-2008 .result-rank').text(rank_list[i]['rank']);
+				}
+			}
+
+		});
+
+	});
+
+});
